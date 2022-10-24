06fdf01ba40433d1f870f3d28516886a964dec2a
Flandre <flantan233@gmail.com>
2022-10-24 17:30:24 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-
-
-
-
-
+const fs = require('fs')
+const path = require('path-extra')
+const compressing = require('compressing');
+const archiver = require('archiver');
+const extract = require('extract-zip')
 
 const init = async () => {
-
+	compressing.zip.uncompress('out/mods/AdPother-1.12.2-1.2.22.0-build.0624.jar', './output/')
 }
+
+const handleError = (e) => {
+	console.log(e)
+}
+init()
